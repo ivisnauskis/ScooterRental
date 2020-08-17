ba4603d77a23db93fdf4d1e82355895464ee2af6
ivisnauskis <invisnauskis@gmail.com>
2020-08-17 10:15:06 +0300

    RentalCompany

---
+++ ScooterRental.tests/RentalCompany.test.cs
@@ -0,0 +1,16 @@
+﻿using ScooterRental.Exceptions;
+using Xunit;
+
+namespace ScooterRental.tests
+{
+    public class RentalCompanyTests
+    {
+        [Fact]
+        public void test()
+        {
+            IRentalCompany company = new RentalCompany();
+        }
+
+    }
+}
+
---
+++ ScooterRental/RentalCompany.cs
@@ -0,0 +1,21 @@
+﻿namespace ScooterRental
+{
+    public class RentalCompany : IRentalCompany
+    {
+        public string Name { get; }
+        public void StartRent(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public decimal EndRent(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public decimal CalculateIncome(int? year, bool includeNotCompletedRentals)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
